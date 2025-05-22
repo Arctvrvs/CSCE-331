89d09b8107ef47865701828fb727a08aca1c0d60
Arctvrvs <134107238+Arctvrvs@users.noreply.github.com>
2025-05-21 19:14:58 -0500

    Add files via upload

--- Week 2/project-calc/src/main.cpp
+++ Week 2/project-calc/src/main.cpp
@@ -17,13 +17,9 @@ int main()
 
     auto safe = safe_div(8,0);
 
-    if (safe)
-    {
-        cout << " 8 / 0 = " << safe << '\n';
-    }
-
-    else
-    {
+    if (safe){
+        cout << " 8 / 0 = " << *safe << '\n';
+    } else{
     cout << "safe_div caught divide by zero.\n";
     }
 
